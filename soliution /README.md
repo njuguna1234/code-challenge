8545a1d67354b772d3e8f708c4de20d5118f44e1
roy wahome <roy.njuguna@student.moringaschool.com>
2024-03-22 11:05:32 +0300

    the grading system

---
+++ soliution /README.md
@@ -0,0 +1,2 @@
+# code-challenge
+the code challenge 
---
+++ soliution /solition1.js
@@ -0,0 +1,23 @@
+let grade = 58
+ 
+function gradingSystem(grade){
+
+    if(grade> 79){
+         
+        console.log("A")
+    }else if(grade>=60){
+
+        console.log("B-")
+    }else if(grade>=49){
+
+        console.log("c-")
+    }else if(grade>=40){
+
+        console.log("D-")
+    }else {
+        console.log("E")
+    }
+
+}
+
+gradingSystem(49)
